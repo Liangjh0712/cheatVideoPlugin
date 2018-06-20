80bab2d17d209afc64644b2a403f43f854a16304
luke liang <liang1360542446@gmail.com>
2018-06-20 13:38:25 +0800

    beta 1.0

--- cheat.js
+++ cheat.js
@@ -1,31 +1,37 @@
 const cheat = (cycleTime = 1) => {
-    // let ele = window.frames['w_main'].frames['w_lms_content'].document.getElementById('tdRemark');
-
-    // console.log(ele);
     try {
-        let tblDataList = window.frames['w_main'].document.querySelector('#frame_learning_content_1 #tblDataList');//frame_learning_content_1
+        let tblDataList = window.frames['w_main'].document.querySelector('#frame_learning_content_1 #tblDataList');
         let incomplete = tblDataList.querySelector('.scorm.incomplete');
         let notattempt = tblDataList.querySelector('.scorm.notattempt');
         let currentElement = incomplete || notattempt;
-        if (currentElement){
-            currentElement.parentElement.parentElement.querySelector('a').onclick();
+        if (currentElement) {
+            currentElement.parentElement.parentElement.querySelector('a').click();
+            cheatVideo(60);
         }
     } catch (e) {
-        console.log(e);
-    } finally {
         setTimeout(cheat, 1000 * cycleTime, cycleTime);
     }
-
-    // if (ele) {
-    //     let text = ele.innerText;
-    //     console.log(text, text.includes('已经学习完毕！'));
-    //     if (text.includes('已经学习完毕！') || text.includes('学习课件完毕')) { // 学习课件完毕
-    //         window.frames['w_main'].frames['w_code'].document.getElementById('btnNext').onclick();
-    //     }
-    // }
 };
 
 cheat(1);
+
+const cheatVideo = (cycleTime = 1) => {
+    try {
+        let ele = window.frames['w_main'].frames['w_lms_content'].document.getElementById('tdRemark');
+        if (ele) {
+            let text = ele.innerText;
+            console.log(text, text.includes('已经学习完毕！'));
+            if (text.includes('已经学习完毕！') || text.includes('学习课件完毕')) { // 学习课件完毕
+                window.frames['w_main'].frames['w_code'].document.getElementById('btnNext').click();
+                window.location.reload();
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        setTimeout(cheatVideo, 1000 * cycleTime, cycleTime);
+    }
+};
 
 
 //.scorm.incomplete     正在学习
